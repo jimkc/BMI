39ace7a2abe5c15055406da0c4d0e80942ccc59c
r03525056 <r03525056@ntu.edu.tw>
2015-11-17 12:24:54 +0800

    modify GpsActivity

--- app/src/main/java/com/example/jimkc19940908/bmigps/GpsActivity.java
+++ app/src/main/java/com/example/jimkc19940908/bmigps/GpsActivity.java
@@ -2,18 +2,18 @@ package com.example.jimkc19940908.bmigps;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 public class GpsActivity extends Activity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
+
+        tv=(TextView)findViewById(R.id.gpstextview);
 
 
     }
